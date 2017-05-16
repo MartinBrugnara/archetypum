80e9194d8f1c8ceaf68b69610be96dece550a28c
Martin Brugnara <martin.brugnara@gmail.com>
2017-05-16 20:10:33 +0200

    Separate qi from reg

--- src/reg.ts
+++ src/reg.ts
@@ -6,11 +6,18 @@ import {CdbMessage} from './cdb'
 export type RegConfig = {ints:number, floats:number};
 
 export class Register {
-    private regs: {[key:string]: string} = {};
+    private regs: {[key:string]: number} = {};
+    private qi: {[key:string]: string | null} = {};
 
     constructor(conf:RegConfig){
-        for(let i=0; i<conf.ints; i++) this.regs[`R${i}`]='0';
-        for(let f=0; f<conf.floats; f++) this.regs[`F${f}`]='0';
+        for(let i=0; i<conf.ints; i++) {
+            this.regs[`R${i}`] = 0;
+            this.qi[`R${i}`] = null;
+        }
+        for(let f=0; f<conf.floats; f++) {
+            this.regs[`F${f}`] = 0;
+            this.qi[`F${f}`] = null;
+        }
     }
 
     patch(ri: RawInstruction):Instruction {
@@ -18,20 +25,20 @@ export class Register {
 
         ins.vj = parseInt(ri.src0, 10);
         if (isNaN(ins.vj)) {                        // then src0 is a reg name
-            ins.vj = parseInt(this.regs[ri.src0], 10)
+            ins.vj = this.regs[ri.src0]
             if (isNaN(ins.vj)) {                    // then we wait for RS
                 ins.vj = null;
-                ins.qj = this.regs[ri.src0];
+                ins.qj = this.qi[ri.src0];
             }
         }
 
         if (ri.src1 !== null) {
             ins.vk = parseInt(ri.src1, 10);
             if (isNaN(ins.vk)) {                     // then src1 is a reg name
-                ins.vk = parseInt(this.regs[ri.src1], 10)
+                ins.vk = this.regs[ri.src1]
                 if (isNaN(ins.vk)) {                 // then we wait for RS
                     ins.vk = null;
-                    ins.qk = this.regs[ri.src1];
+                    ins.qk = this.qi[ri.src1];
                 }
             }
         }
@@ -40,13 +47,16 @@ export class Register {
     }
 
     setProducer(inst: Instruction, rs: string) {
-        this.regs[inst.dst] = rs;
+        this.regs[inst.dst] = 0;
+        this.qi[inst.dst] = rs;
     }
 
     readCDB(cdb: Queue<CdbMessage>): void {
         for (let msg of cdb) {
-            if (this.regs[msg.dst] === msg.rsName) // if were still waiting for it
-                this.regs[msg.dst] = msg.result.toString();
+            if (this.qi[msg.dst] === msg.rsName) { // if were still waiting for it
+                this.regs[msg.dst] = msg.result;
+                this.qi[msg.dst] = null;
+            }
         }
     }
 }
